df0767b163f1c9ee49c23f74547ef33cfe0b0a1c
glebson1988 <glebson1988@yandex.ru>
2019-08-13 11:27:18 +0300

    code from screencast User can login in system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'questions#index'
+
   resources :questions, only: %i[new create show] do
     resources :answers, only: %i[new create show], shallow: true
   end
