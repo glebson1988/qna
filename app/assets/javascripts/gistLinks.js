4e66c20b836aa5c42e6e90fe4c9ba0068ea756cb
Gleb Grishakov <glebson1988@yandex.ru>
2019-10-06 21:52:56 +0300

    gists show by js and setBest bug

--- app/assets/javascripts/gistLinks.js
+++ app/assets/javascripts/gistLinks.js
@@ -1,6 +1,27 @@
 $(document).on('turbolinks:load', function () {
-    $('.link.link-gist').each(function(){
-        var gist_id = $(this).data('id');
-        $(this).replaceWith(`<a href="https://https://api.github.com/gists/${gist_id}.json"></a>`);
-    })
+    $('.link-gist').each(function () {
+        let gist_id = $(this).data('id'),
+            gist_url = `https://api.github.com/gists/${gist_id}`,
+            that = this;
+            // gist_script = `https://gist.github.com/${gist_id}.js`,
+            // script = jQuery.getScript(gist_script),
+
+        // $(this).replaceWith(postscribe($(this), script));
+
+        // $.getJSON(gist_url, (data =>
+        //     Object.keys(data.files).forEach(item =>
+        //     $(that).replaceWith(data.files[item].content))));
+
+        fetch(gist_url)
+            .then(response => {
+                if (response.status >= 400) {
+                    return Promise.reject('Не удалось загрузить gist');
+                }
+
+                response.json().then(function (data) {
+                    Object.keys(data.files).forEach(item =>
+                        $(that).replaceWith(data.files[item].content));
+                });
+            });
+    });
 });
