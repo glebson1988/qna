6cf027af3ea73e99d0c1b8b360d702ebe9e41a4d
glebson1988 <glebson1988@yandex.ru>
2019-11-01 15:39:54 +0300

    code from screencasts of lesson12 without last scrncst

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :rewards, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -10,9 +11,31 @@ class User < ApplicationRecord
          :registerable,
          :recoverable,
          :rememberable,
-         :validatable
+         :validatable,
+         :omniauthable, omniauth_providers: [:github]
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+    user
+  end
 
   def author_of?(item)
     id == item.user_id
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
