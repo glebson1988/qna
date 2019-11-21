2c96515efce73c7e0a8fa5b5bbff089c38b6ca50
glebson1988 <glebson1988@yandex.ru>
2019-11-21 16:53:06 +0300

    api list of all users without authenticated and user serializer

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -33,4 +33,41 @@ describe 'Profiles API', type: :request do
       end
     end
   end
+
+  describe 'GET api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:users) { create_list(:user, 4) }
+      let(:me) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'return 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'return all profiles without me' do
+        expect(json.size).to eq(users.size - 1)
+        expect(json).to_not include me
+      end
+
+      it 'returns all public fields' do
+        %w[id email admin created_at updated_at].each do |attr|
+          expect(json.last[attr]).to eq users.last.send(attr).as_json
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(json.last).to_not have_key(attr)
+        end
+      end
+    end
+  end
 end
