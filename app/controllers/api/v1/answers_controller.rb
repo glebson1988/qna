baf419c1821598c0ece8f1d89720db159520a8b4
glebson1988 <glebson1988@yandex.ru>
2019-11-26 07:25:27 +0300

    api separate answer with serializer

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -2,15 +2,24 @@ class Api::V1::AnswersController < Api::V1::BaseController
 
   authorize_resource class: Answer
 
-  before_action :find_question, only: :index
+  before_action :question, only: :index
+  before_action :answer, only: %i[show]
 
   def index
     render json: @question.answers
   end
 
+  def show
+    render json: answer, serializer: AnswerSeparateSerializer
+  end
+
   private
 
-  def find_question
+  def question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer
+    @answer = Answer.find(params[:id])
   end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -33,4 +33,51 @@ describe 'Answers API', type: :request do
       end
     end
   end
+
+    describe 'GET /api/v1/answers/:id' do
+      let(:user) { create(:user) }
+      let(:question) { create(:question, :with_attachment, user: user) }
+      let(:answer) { create(:answer, :with_attachment, question: question, user: user) }
+      let(:answer_response) { json['answer'] }
+      let!(:comments) { create_list(:comment, 3, commentable: answer, user: user) }
+      let!(:links) { create_list(:link, 3, linkable: answer) }
+      let(:access_token) { create(:access_token) }
+      let(:api_path) { "/api/v1/answers/#{answer.id}" }
+
+      it_behaves_like 'API Authorizable' do
+        let(:method) { :get }
+        let(:api_path) { '/api/v1/questions' }
+      end
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it_behaves_like 'Request successful'
+
+      it_behaves_like 'Public fields' do
+        let(:attrs) { %w[id body created_at updated_at] }
+        let(:resource_response) { answer_response }
+        let(:resource) { answer }
+      end
+
+      describe 'comments' do
+        it_behaves_like 'Return list' do
+          let(:resource_response) { answer_response['comments'] }
+          let(:resource) { comments }
+        end
+      end
+
+      describe 'links' do
+        it_behaves_like 'Return list' do
+          let(:resource_response) { answer_response['links'] }
+          let(:resource) { links }
+        end
+      end
+
+      describe 'files' do
+        it_behaves_like 'Return list' do
+          let(:resource_response) { answer_response['files'] }
+          let(:resource) { answer.files }
+      end
+    end
+  end
 end
