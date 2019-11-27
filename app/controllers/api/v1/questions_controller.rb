5b3c6ec4e60cc9fb3052427078606379c29244c7
Gleb Grishakov <glebson1988@yandex.ru>
2019-11-27 21:45:27 +0300

    hw14 att1

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   before_action :find_question, only: %i[show update destroy]
 
   def index
-    @questions = Question.all
+    @questions = Question.includes(:answers).limit(10)
     render json: @questions
   end
 
