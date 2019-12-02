c1444d3f287eebb21284fdc359fc38fca8f1afd5
glebson1988 <glebson1988@yandex.ru>
2019-12-02 15:01:43 +0300

    daily digest list of all questions for a day

--- app/mailers/daily_digest_mailer.rb
+++ app/mailers/daily_digest_mailer.rb
@@ -1,7 +1,7 @@
 class DailyDigestMailer < ApplicationMailer
 
   def digest(user)
-    @greeting = "Hi"
+    @questions = Question.where(created_at: Date.today - 1)
 
     mail to: user.email
   end
--- spec/mailers/daily_digest_spec.rb
+++ spec/mailers/daily_digest_spec.rb
@@ -2,17 +2,25 @@ require "rails_helper"
 
 RSpec.describe DailyDigestMailer, type: :mailer do
   describe "digest" do
-    let(:mail) { DailyDigestMailer.digest }
+    let(:user) { create(:user) }
+    let(:mail) { DailyDigestMailer.digest(user) }
+    let(:questions_yesterday) { create_list(:question, 2, :created_at_yesterday) }
+    let(:questions_more_yesterday) { create_list(:question, 2, :created_at_more_yesterday) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Digest")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders only yesterday's questions" do
+      questions_yesterday.each do |question|
+        expect(mail.body.encoded).to match question.title
+      end
+
+      questions_more_yesterday.each do |question|
+        expect(mail.body.encoded).to_not match question.title
+      end
     end
   end
-
 end
