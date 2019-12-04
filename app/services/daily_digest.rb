f2047686282e119486893eceaa8ca5bc7fd3984e
glebson1988 <glebson1988@yandex.ru>
2019-12-04 16:27:23 +0300

    fix Services::DailyDigest#self.call

--- app/services/daily_digest.rb
+++ app/services/daily_digest.rb
@@ -1,6 +1,6 @@
 class Services::DailyDigest
   def self.call
-    return unless Question.where(created_at: Date.yesterday.all_day).empty?
+    return if Question.where(created_at: Date.yesterday.all_day).empty?
 
     User.find_each { |user| DailyDigestMailer.digest(user).deliver_later }
   end
