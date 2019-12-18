3db8f50f81248f65fca017540c4c98e5ebf4cbb0
glebson1988 <glebson1988@yandex.ru>
2019-12-18 14:37:38 +0300

    hw20 att2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,4 @@ module ApplicationHelper
         alert: 'alert-danger'
     }[key.to_sym]
   end
-
-  def collection_cache_key_for(model)
-    klass = model.to_s.capitalize.constantize
-    count = klass.count
-    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
-  end
 end
